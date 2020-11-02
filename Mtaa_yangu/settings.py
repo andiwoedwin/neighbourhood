84dbd99eb8b733443696614aa43a63192e9ca7d4
andiwoedwin <andiwoedwin@gmail.com>
2020-11-02 20:54:15 +0300

    configure whitenoise

--- Mtaa_yangu/settings.py
+++ Mtaa_yangu/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-# import django_heroku
-# from decouple import config, Csv
-# import dj_database_url
+import django_heroku
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Andiwo'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -148,4 +148,4 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
